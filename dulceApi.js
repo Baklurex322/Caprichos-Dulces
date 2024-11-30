4bdc6d11df77bed68bbf5dfcc688cdf970e30c9c
OokamiT16 <150981243+OokamiT16@users.noreply.github.com>
2024-11-29 18:29:57 -0700

    Add files via upload

--- dulceApi.js
+++ dulceApi.js
@@ -1,104 +1,112 @@
 const http = require("node:http");
 const fs = require("node:fs");
-const { json } = require("stream/consumers");
 
 const puerto = 3001;
 
-const archivoItems = fs.readFileSync("./json/productos.json", "utf8");
-var objetoItems = JSON.parse(archivoItems);
-//console.log(objetoItems);
+const archivoItems = fs.readFileSync("./json/items.json", "utf8");
+const objetoItems = JSON.parse(archivoItems);
 
 const server = http.createServer((request, response) => {
-
     response.setHeader("Access-Control-Allow-Origin", "*");
-            
     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-            
     response.setHeader("Access-Control-Allow-Header", "Content-Type");
-        
-    switch(request.method)
-    {
+
+    console.log(request.method);
+    switch(request.method){
         case "GET":
-            
-            response.statusCode = 200;
-            response.setHeader("Content-Type", "application/json; charset=utf-8");
-            response.end(JSON.stringify(objetoItems.ListaProductos));
+            if(request.url == "/"){
+                //console.log(objetoItems);
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "application/json; charset=utf-8");
+                response.end(JSON.stringify(objetoItems.arreglo_items));
+            }
+            else{
+                
+            }
         break;
         case "POST":
-            //aqui vamos a insertar info
-            
-             // el on fucniona para verificar si se esta mandando informacion
-            request.on("data", info => {
-                
-                var objNewItem = JSON.parse(info.toString());
-                objetoItems.ListaProductos.push(objNewItem);
+            if(request.url == "/"){
+                var informacion = "";
+                request.on("data", info =>{
+                    informacion += info.toString();
+                });
+                request.on("end", info => {
+                    console.log("termine de leer");
+                    var objetoAgregarPersona = JSON.parse(informacion);
+                    objetoAgregarPersona.id = objetoItems.arreglo_items.length;
+                    objetoItems.arreglo_items.push(objetoAgregarPersona);
 
-                fs.writeFileSync("./json/productos.json", JSON.stringify(objetoItems)); //el fs.writeFileSync permite guardar de manera constamnte los datos ingresados, osea aun reiniciiando server y pagina deben de seguir ahi los datos
+                    fs.writeFileSync("./json/items.json", JSON.stringify(objetoItems), "utf8");
 
-                var OkResponse = {
-                    "mensaje" : "Todo se encuentra bien"
-                }
+                    response.statusCode = 200;
+                    response.setHeader("Content-Type", "application/json; charset=utf-8");
+                    response.end(JSON.stringify({"respuesta": "persona agregada correctamente"}));
+
+
+                    /*
+                    const matches = informacion.objetoAgregarPersona.imagen.match(/^data:(.+);base64,(.+)$/);
+                    const buffer = Buffer.from(matches[2], 'base64');
+                    fs.writeFileSync("./img/imagenPrueba.png", buffer);
+                    */
+                })
+
+            }
+            else{
+                var informacion = ""
+                request.on("data", info =>{
+                    informacion += info;
+                });
+                request.on("end", info =>{
+                    const matches = informacion.match(/^data:(.+);base64,(.+)$/);
+                    const buffer = Buffer.from(matches[2], 'base64');
+                    fs.writeFileSync("./img/imagenPrueba.png", buffer);
+
+                    /*response.statusCode = 200;
+                    response.setHeader("Content-Type", "application/json; charset=utf-8");
+                    response.end(JSON.stringify({}));*/
+                });
+            }
+        break;
+        case "PUT":
+            var informacion = ""
+            request.on("data", info =>{
+                informacion += info.toString();
+            });
+            request.on("end", info =>{
+                console.log("termine de leer");
+                var objetoModificarPersona = JSON.parse(informacion);
+                objetoItems.arreglo_items[objetoModificarPersona.id] = objetoModificarPersona;
+                fs.writeFileSync("./json/items.json", JSON.stringify(objetoItems), "utf8");
 
                 response.statusCode = 200;
                 response.setHeader("Content-Type", "application/json; charset=utf-8");
-                response.end(JSON.stringify(objetoItems.ListaProductos));
-
+                response.end(JSON.stringify({"respuesta": "persona modificada correctamente"}));
             });
-
-        break;
-        case "PUT":
-            //aqui estamos mandando informacion
-            var informacion = "";
-            request.on("data", info => {
-                informacion += info.toString(); //aqui manda y guarda la info por cachos, ya que es mucha y pesada. La divide y alamcena enuna variable
-            });
-            request.on("end", info => {
-
-                //aqui vamos a transformar la info recibida a objeto
-                var objModItem = JSON.parse(informacion);
-
-                //aqui vamos a modificar el JSON archivoItems con la nueva info de ObjModItems
-                
-                objetoItems.ListaProductos[0] = objModItem;
-                //aqui se debe guardar la info que cambio en la web modificando el json que tenemos
-                fs.writeFileSync("./json/items.json", JSON.stringify(objetoItems), "utf8");
-            });
-
         break;
         case "DELETE":
+            console.log("Estas intentando borrar a alguien");
+            request.on("data", info =>{
+                var objetoItemABorrar = JSON.parse(info.toString());
 
-            request.on("data", info => {
-                console.log(info.toString());
-                var objdel = JSON.parse(info.toString());
+                objetoItems.arreglo_items.splice(objetoItemABorrar.id, 1);
 
-                objetoItems.ListaProductos.splice(objdel.id, 1); //aqui es la linea que borra, el .splice borra un punto especifico .splice(variable.id del elemento, cuantos elementos van a borrar)
+                console.log(objetoItems.arreglo_items);
+                fs.writeFileSync("./json/items.json", JSON.stringify(objetoItems), "utf8");
 
-                fs.writeFileSync("./js/items.json", JSON.stringify(objetoItems), "utf8");
-
-                var OkResponse = {
-                    "mensaje" : "mensaje borrado"
-                }
-            response.statusCode = 200;
-            response.setHeader("Content-Type", "application/json; charset=utf-8");
-            response.end(JSON.stringify(objetoItems.ListaProductos));
-
-            })
-
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "application/json; charset=utf-8");
+                response.end(JSON.stringify({"respuesta": "elemento eliminado satisfactoriamente"}));
+            });
         break;
-        //OPTIONS se usa porque no dejaba usar delete o put
         case "OPTIONS":
-            //aqui se va a mandar una respuesta HHTP204, (que se espere basicamente) 
             response.writeHead(204);
             response.end();
-        break;
-
-    }
-
-   
             
+            break;
+    }
     
 });
 
 server.listen(puerto, () => {
-    console.log("Servidor encendido y a la escucha en: http://localhost:" + puerto);
+    console.log("Servidor encendido ejecutandose en http://localhost:" + puerto);
 });
--- js/consultarItems.js
+++ js/consultarItems.js
@@ -1,163 +1,129 @@
-var arregloPanelesEdicion;
+// Obtener Items
+const plantillaItem = document.querySelector(".ContenedorItem");
+const contenedorItems = document.querySelector("#ColeccionItems");
 
-fetch("http://localhost:3001/").then(recursos => recursos.json()).then(archivo => {
-    console.log(archivo);
+// Pantalla Edicion
+var arregloPanelesCreacion;
 
-    const platillaItem = document.querySelector(".ContenedorItem");
-    const contenedorItems = document.querySelector("#ColeccionItems");
+fetch("http://localhost:3001").then(recurso => recurso.json()).then(archivo => {
 
-    for(i = 0; i < archivo.length -1; i++){
-        var clon = platillaItem.cloneNode(true);
+    for(i = 0; i < archivo.length; i++){
+        var clon = plantillaItem.cloneNode(true);
         contenedorItems.appendChild(clon);
     }
 
-    var arregloItems = document.querySelectorAll(".ContenedorItem");
-    arregloPanelesEdicion = document.querySelectorAll(".PanelCreacionItem");
+    const arregloItems = document.querySelectorAll(".ContenedorItem");
+    console.log(arregloItems.length);
 
-    for(i = 0; i < arregloItems.length; i++){
+    console.log(archivo.length);
+    for(i = 0; i < archivo.length; i++){
 
-        //Obtener Campos items
-        const titulo = arregloItems[i].querySelector(".nombreProducto");
+        // Cambiar los valores del Item
+        const titulo = arregloItems[i].querySelector(".Titulo");
         titulo.innerHTML = archivo[i].titulo;
-
         const descripcion = arregloItems[i].querySelector(".Descripcion");
         descripcion.innerHTML = archivo[i].descripcion;
+        const imagen = arregloItems[i].querySelector(".Imagen");
 
-        const imagen = arregloItems[i].querySelector(".img-producto");
         imagen.style.backgroundImage = "url(" + archivo[i].imagen + ")";
 
-        const precio = arregloItems[i].querySelector(".costo");
-        precio.innerHTML = archivo[i].precio;
-
-        const cantidad = arregloItems[i].querySelector(".costo");
-        cantidad.innerHTML = archivo[i].cantidad;
-
-
-
-        //PANTALLAS DE EDICION
-        //ArregloPanelesEdicion[i].style.display = "flex";
-        //console.log(arregloPanelesEdicion);
+        // Funcionalidad modificar
         const botonEditar = arregloItems[i].querySelector(".Icono_Editar");
-        botonEditar.tag = i; //tag es una variable cualquiera, solo que aqui se llamó tag
+        botonEditar.tag = i;
         botonEditar.addEventListener("click", function(evento){
-            //arregloPanelesEdicion[i].style.display = "flex"; 
-            //console.log(evento.currentTarget.tag);
-            arregloPanelesEdicion[evento.currentTarget.tag].style.display = "flex";
-        })
-
-        
-         //boton de actualizar
-        const botonActualizarCambios = arregloPanelesEdicion[i].querySelector("button");
-        botonActualizarCambios.tag2 = i;
-        botonActualizarCambios.addEventListener("click", function(evento)
-        {
-
-            botonGuardarCambiosPress(evento.currentTarget.tag2);
-
-        })
-
-        //botonde borrar
-        const BorrarBtn = arregloItems[i].querySelector(".Icono_Borrar");
-        BorrarBtn.tag = i;
-        BorrarBtn.addEventListener("click", function(evento)
-        {
-        alert("Borrar el producto:  " + evento.currentTarget.tag); //con evento.currentTarget.tag te dice el elemento actual
-        fetch("http://localhost:3001", {
-            method: "DELETE",
-            body: JSON.stringify(objdel)
-            }).then(respuesta => 
-                {
-            alert("Elemento eliminado");
-            location.reload();
-
-            });
-
-        })
-        
-        
-
-
-    }
-
-    //agregar el botón de agregar item hasta el final de la lista
-    const botonAgregarItem = document.querySelector(".PantallaAgregarItem");
-    contenedorItems.appendChild(botonAgregarItem);
-
-
-});
-
-function botonGuardarCambiosPress(numero)
-{
-
-    //titulo
-    const campTitulo = arregloPanelesEdicion[numero].querySelector(".inputTitulo");
-    //campTitulo.value = "texto Generico";  //value se peude usar en etiquetas que no requieren cerrarse, como <img>
-    //console.log(campTitulo);
-
-    //descripcion
-    const campDescripcion = arregloPanelesEdicion[numero].querySelector("textarea");
-    console.log(campDescripcion.value);
-    //campDescripcion.innerHTML = "a chiquita"; //Inner.HTML sirve para insertar info en etiqeutas que se abren y cierran como <div></div> 
-
-    //img
-    const campImg = arregloPanelesEdicion[numero].querySelector(".input_imagen");
-    var archivo = campImg.files[0];
-    var reader = new FileReader();//aqui es para meter archivo
-    reader.readAsDataURL(archivo);
-    reader.onload =  function(){
-        console.log(reader.result);
-
-        //trasnformar toda la info en JaSon
-        const nuevoItem = {
-            "titulo": campTitulo.value,
-            "descripcion" : campDescripcion.value,
-            "imagen" : reader.result
-        }
-
-        //Aqui modificamos info
-        //Para mandar info se debe convertir a texto, y para recibir informacion se debe trnasformar a Json
-        fetch("http://localhost:3001", {
-            method: "PUT",
-            body : JSON.stringify(nuevoItem)
-
-
+            mostrarPanelEditar(evento.currentTarget.tag);
+            
         });
 
-        location.reload(); //esta linea recarga el sitio cada que se guarde un cambio
-        //console.log(nuevoItem);
+        const botonActualizar = arregloItems[i].querySelector("button");
+        botonActualizar.tag = i;
+        botonActualizar.addEventListener("click", function(evento){
+            guardarCambios(evento.currentTarget.tag);
+        })
+        const imagenActualizar = arregloItems[i].querySelector("img");
+
+        // Funcionalidad borrar Item
+        const botonEliminar = arregloItems[i].querySelector(".Icono_Borrar");
+        botonEliminar.tag = i;
+        botonEliminar.addEventListener("click", function(evento){
+            borrarItem(evento.currentTarget.tag);
+        });
+
     }
+    arregloPanelesCreacion = document.querySelectorAll(".PanelCreacionItem");
+    arregloItems[arregloItems.length - 1].style.display = "none";
 
-   
-    
+    arregloItems[arregloItems.length - 1].querySelector(".PanelCreacionItem").style.display = "flex";
 
-    /*alert("Haz guardado un cambio en la tarjeta no: " + evento.currentTarget.tag2);
-    //alert(campTitulo.value); con este imprimimos el valor que ingresa el usuario en el campod designado
-    alert(campDescripcion.innerHTML);*/
+    // Poner la pantalla de creación hasta el final
+    const pantallaCreacion = document.querySelector(".PantallaAgregarItem");
+    //var clon = pantallaCreacion.cloneNode(true);
+    contenedorItems.appendChild(pantallaCreacion);
+})
 
-
+function crearNuevoItem(){
+    const objetoNuevoItem = {
+        "id": -1,
+        "titulo": "...",
+        "descripcion": "...",
+        "imagen": "https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+    }
+    fetch("http://localhost:3001/", {method: "POST", body: JSON.stringify(objetoNuevoItem)}).then(archivo => archivo.text()).then(texto => {
+        location.reload();
+    });
 }
- //aqui usamos un boton conectado con el htlm
- function ClickAgregarItm()
- {
 
-     //aqui vamos a insertar info
-     var newItem2 = {
-         "id" : -1,
-         "titulo": "...",
-         "descripcion" : "...",
-         "img" : "https://preview.redd.it/no-hace-nada-v0-dil3g2kfdbid1.png?auto=webp&s=86576347e637f0772c1f768bcf1f4a2fc893ce9a"
+function guardarCambios(numeroBoton){
+    var input = arregloPanelesCreacion[numeroBoton].querySelector(".input_imagen")
+    //var imagen = arregloPanelesCreacion[numeroBoton].querySelector(".PanelCreacionItem img");
 
-     }
+    const campoTitulo = arregloPanelesCreacion[numeroBoton].querySelector("input");
+    const campoDescripcion = arregloPanelesCreacion[numeroBoton].querySelector("textarea");
+    
+    var archivo = input.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(archivo);
+    reader.onload = function(){
+        //imagen.src = reader.result;
 
-     fetch("http://localhost:3001", {
-         method: "POST",
-         body : JSON.stringify(newItem2)
+        const objetoNuevoItem = {
+            "id": numeroBoton,
+            "titulo": campoTitulo.value,
+            "descripcion": campoDescripcion.value,
+            "imagen": reader.result
+        }
+        console.log(objetoNuevoItem);
 
-
-     }).then(respuesta => { //este .then se realiza una vez que toda la funcion terminó, es como una funcion que se realiza a continuacion de la pasada solo si lo hizo de manera correcta
-            alert("pche cosa inutil, ahi ta");
+        fetch("http://localhost:3001/", {method: "PUT", body: JSON.stringify(objetoNuevoItem)}).then(archivo => archivo.text()).then(texto => {
             location.reload();
-     });
-     
+        });
+    }
+}
 
- }
+function mostrarPanelEditar(numeroBoton){
+    console.log(arregloPanelesCreacion[numeroBoton]);
+    arregloPanelesCreacion[numeroBoton].style.display = "flex";
+}
+
+var inputPrueba = document.querySelector(".input_prueba");
+inputPrueba.addEventListener("change", function (){
+    alert("hola");
+
+    var archivo = inputPrueba.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(archivo);
+    reader.onload = function(){
+        fetch("http://localhost:3001/prueba", {method: "POST", body: reader.result}).then(archivo => archivo.text()).then(texto => {
+            location.reload();
+        });
+    }    
+});
+
+function borrarItem(numeroBoton){
+    var objetoBorrarItem = {
+        "id": numeroBoton
+    }
+    fetch("http://localhost:3001/", {method: "DELETE", body: JSON.stringify(objetoBorrarItem)}).then(archivo => archivo.text()).then(texto => {
+        location.reload();
+    });
+}
